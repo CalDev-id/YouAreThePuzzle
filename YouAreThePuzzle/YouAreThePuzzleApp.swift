09937b878aa61f500f8fa7ead274942c4960608e
Heical Chandra <haical31@gmail.com>
2024-05-31 11:02:45 +0700

    Initial commit

---
+++ YouAreThePuzzle/YouAreThePuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreThePuzzleApp.swift
+//  YouAreThePuzzle
+//
+//  Created by Heical Chandra on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreThePuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
